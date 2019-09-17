902bb0e7b29520693003ffc859bb6c233918a7d9
mrprokoala <zunigaruizb@gmail.com>
2019-09-17 09:21:23 +0800

    fix: fixed weapon class

--- Weapon.cs
+++ Weapon.cs
@@ -6,14 +6,13 @@ namespace the_aztec_game
     class Weapon : Item
     {
         public double dmg { get; set; }
-        public double stamina { get; set; }
 
         public Weapon()
         {
 
         }
 
-        public double getRandDmg()
+        override public double getRandDmg()
         {
             Random r = new Random();
             return (r.NextDouble() + 0.5) * dmg;
